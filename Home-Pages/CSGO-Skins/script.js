1984b86e9fc268baa2cfbc4b88321396092c0b77
Boburjon <shadow88bob@gmail.com>
2023-05-10 16:08:08 +0500

    add pages

--- Home-Pages/CSGO-Skins/script.js
+++ Home-Pages/CSGO-Skins/script.js
@@ -1,7 +1,8 @@
 const nav = document.querySelector(".header__nav");
 const btn = document.querySelector(".header__burger");
-const lists = document.querySelectorAll(".header__list")
-const body = document.querySelector('body')
+const lists = document.querySelectorAll(".header__list");
+const body = document.querySelector('body');
+const btnSign = document.querySelector(".btn-sign")
 
 btn.addEventListener("click", () => {
     nav.classList.toggle("open");
@@ -17,3 +18,10 @@ lists.forEach((elem) => {
         nav.classList.toggle("open");
     });
 });
+
+btnSign.addEventListener("click", () => {
+    function openPage() {
+        window.location.href = '.././Registration/CSGO-Login/';
+    };
+    openPage();
+})
